5c3331cf7de613b65d4c0838ffb6a19888aff51c
Sudip <xthazudp@gmail.com>
2023-04-22 19:55:57 +0545

    increse and decrease amount

--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,26 @@ const reducer = (state, action) => {
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
     };
   }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount != 0);
+    return { ...state, cart: tempCart };
+  }
   return state;
 };
 
